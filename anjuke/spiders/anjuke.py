a14d2085147eeb1b2ec20be0ab3e08a4dc94c4ca
Yang Xiaoyu <56571660+clearRM@users.noreply.github.com>
2024-11-11 15:07:45 +0800

    Update anjuke.py

--- anjuke/spiders/anjuke.py
+++ anjuke/spiders/anjuke.py
@@ -2,10 +2,9 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-from scrapy.spiders import CrawlSpider, Rule
 # 导入链接规则匹配类，用来提取符合规则的连接
-from scrapy.linkextractors import LinkExtractor
 from anjuke.items import AnjukeItem
+
 
 class AnjukeSpider(CrawlSpider):
     name = 'anjuke'
@@ -20,7 +19,6 @@ class AnjukeSpider(CrawlSpider):
 
     def parse_item(self, response):
         for each in response.xpath('//div[@class="zu-info"]'):
-
             item = AnjukeItem()
             titleold = each.xpath('h3').xpath("string(.)").extract()[0]
             title = self.zhuanma(titleold)
@@ -28,11 +26,9 @@ class AnjukeSpider(CrawlSpider):
             sizeold = each.xpath('p[@class="details-item tag"]/b/text()').extract()[0]
             size=self.zhuanma(sizeold)
             #content.replace(u'\xa0', u'')
-
             item['title'] = title.strip()
             item['link'] = link.strip()
             item['size'] = size.strip()
-
 
             yield item
     def zhuanma(self,mm):
